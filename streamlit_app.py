8cf5bf2f48ea14737b7ffa08186bb91f1f034f65
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-02-26 19:12:28 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,7 +118,7 @@ def app():
         classify(clf)
         visualize_classifier(clf, X_test, y_test_pred)
 
-def classify(clf):
+def classify(clf, X_train, X_test, y_train, y_test):
         clf.fit(X_train,y_train)
         y_test_pred = clf.predict(X_test)
         st.subheader('Confusion Matrix')
